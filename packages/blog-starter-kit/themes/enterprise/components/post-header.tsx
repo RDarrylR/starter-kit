aa87a21d9436553c858d53db3b79b8cfd6a7336d
Darryl Ruggles <1441671+RDarrylR@users.noreply.github.com>
2025-11-18 13:12:18 -0500

    Update Avatar component size and picture source

--- packages/blog-starter-kit/themes/enterprise/components/post-header.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/post-header.tsx
@@ -25,8 +25,8 @@ export const PostHeader = ({ title, coverImage, date, author, readTimeInMinutes 
 				<Avatar
 					username={author.username}
 					name={author.name}
-					size={1}
-					picture={"https://kagi.com/proxy/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f4239364c6844346630344d6867413d3d2d3635393730363731382e313661363966333030633064383634333939313435373933353137312e6a7067?c=1JAkVVrnOTa6XJatr6D8xHzY21KE_NnG5s38sySa5XdBxNrgewUI6KjQgY3MhVTnSYW9y1rMUbsvpfzMJaDXCGvxm7fbPMxScisZfSPQiQre4tr6A9bXdqztXAnEdtmNSPWAUvnSmaOvXdvgnwudbG4SR-2jdRXMDVZM5aHLNPLAb4mJyyRbHbbSyu9rh4HBnFi2hI_BSSxlyfA85-rxzh-eapRToM1LWE8CnqPX8C6EETeSqIumZwRF-AFGewf9WVMgOgMjdVBHcjsN0DbHKdMwDXQSbGjPL8SZKtppZ7V9W8RVCwvcyuNVr0wtxt_x4CmsDOwnAQlJwK1L1V0VQnGlg3w27AcsKFh7JYlO8VGX3mWmxbxiQPpnUyHzZxnMB9aemEqpPOjT8sfHDUudWzqAFkVQE9mlQCZmfvDA2bE%3D"}
+					size={10}
+					picture={author.profilePicture}
 				/>
 				<span className="block font-bold text-slate-500">&middot;</span>
 				<DateFormatter dateString={date} />
